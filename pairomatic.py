c1dd08c72fb31e6840fb44e636984c6acaa9f899
Erin Bennett <erindb@stanford.edu>
2017-01-21 14:00:56 -0800

    add a license

--- pairomatic.py
+++ pairomatic.py
@@ -1,3 +1,8 @@
+"""
+Copyright © 2015 Erin D Bennett
+See the end of the file for license conditions.
+"""
+
 import json
 import random
 from time import strftime
@@ -229,3 +234,20 @@ def send_emails(pairs, data):
 		emailPair(personA, personB)
 
 main()
+
+"""
+This file is part of pairomatic.
+
+Pairomatic is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+GNU Affero Emacs is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with pairomatic.  If not, see <http://www.gnu.org/licenses/>.
+"""
